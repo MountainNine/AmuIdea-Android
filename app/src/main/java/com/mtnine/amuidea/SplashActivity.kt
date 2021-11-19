0a750177585ab19a30d5c3ad925e3a5511af3da0
MountainNine <goosan126@gmail.com>
2021-11-19 12:44:58 +0900

    add SplashActivity.kt

--- app/src/main/java/com/mtnine/amuidea/SplashActivity.kt
+++ app/src/main/java/com/mtnine/amuidea/SplashActivity.kt
@@ -1,5 +1,17 @@
 package com.mtnine.amuidea
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_splash)
+        Thread.sleep(1000)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
